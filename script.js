f95f20d3c06ae24d58cbb532674a2746e3cad813
buckldav <runalldayandnight@gmail.com>
2021-08-17 15:12:54 -0600

    Disclosure link

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-document.getElementById("courseName").innerHTML = window.location.pathname
+const courseSlug = window.location.pathname
   .split("/")
   .pop()
   .split(".")[0]
   .split("-")
   .map((val) => {
-    if (val === "and") return val;
+    if (val === "and") return "&";
     return val[0].toUpperCase() + val.substring(1, val.length);
   })
   .join(" ");
+document.getElementById("courseName").innerHTML = courseSlug;
+document
+  .getElementById("disclosure")
+  .setAttribute(
+    "href",
+    window.parent.location !== window.location
+      ? `${window.parent.location}/pages/disclosure-document`
+      : `https://cs.meritacademy.tech/#/disclosure?name=${courseSlug}`
+  );
