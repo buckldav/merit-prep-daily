b1801dced4cf8a0578cb303512514c4b3456030a
David Buckley <runalldayandnight@gmail.com>
2022-01-01 09:08:18 -0700

    Game Dev & Semester 2

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ document.getElementById("courseName").innerHTML = courseName.includes(
   : courseName;
 const getCanvasUrl = (slug) => {
   const courses = {
-    "ap-computer-science-a": 967,
-    "ap-computer-science-principles": 951,
+    "ap-computer-science-a": 971,
+    "ap-computer-science-principles": 956,
     "app-and-web-development-1": 1143,
-    "computer-programming-1": 959,
-    "exploring-computer-science": 955,
-    "web-development-capstone": 1142,
+    "computer-programming-1": 963,
+    "exploring-computer-science": 896,
+    "game-development-1": 1144,
+    "web-development-capstone": 1141,
   };
   if (!courseSlug in Object.keys(courses) || top === self) {
     return `https://cs.meritacademy.tech/#/disclosure?name=${slug}`;
