785c692bb73e19e77df4473bccdf29ea621038e5
David Buckley <runalldayandnight@gmail.com>
2022-08-11 21:47:02 -0600

    disclosure url fixed

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
-const courseName = window.location.pathname
-  .split("/")
-  .pop()
-  .split(".")[0]
-  .split("-")
-  .map((val) => {
-    if (val === "and") return "&";
-    return val[0].toUpperCase() + val.substring(1, val.length);
-  })
-  .join(" ");
-const courseSlug = courseName.split(" ").join("-").toLowerCase();
-document.getElementById("courseName").innerHTML = courseName.includes(
-  "Ap Computer Science"
-)
-  ? courseName.replace("Ap Computer Science", "AP CS")
-  : courseName;
-const getCanvasUrl = (slug) => {
-  const courses = {
-    "ap-computer-science-a": 971,
-    "ap-computer-science-principles": 1486,
-    "computer-programming-1": 963,
-    "computer-science-principles": 896,
-    "game-development-1": 1144,
-    "web-development-1": 1143,
-    "web-development-capstone": 1141,
-  };
-  const public = `https://cs.meritacademy.tech/#/disclosure?name=${slug}`;
-  let url =
-    courseSlug in Object.keys(courses)
-      ? "https://meritacademy.instructure.com/courses/${courses[slug]}/pages/disclosure-document"
-      : public;
-  fetch(url, {
-    method: "HEAD",
-  })
-    .then(() => {
-      return url;
-    })
-    .catch((e) => {})
-    .finally(() => {
-      return public;
-    });
+const courses = {
+  "ap-computer-science-a": 1495,
+  "ap-computer-science-principles": 1486,
+  "computer-programming-1": 1505,
+  "computer-science-principles": 1587,
+  "game-development-1": 1590,
+  "web-development-1": 1143,
+  "web-development-capstone": 1398,
 };
-document
-  .getElementById("disclosure")
-  .setAttribute("href", getCanvasUrl(courseSlug));
 
-console.log(window);
+const getCanvasUrl = async () => {
+  // title case
+  const courseName = window.location.pathname
+    .split("/")
+    .pop()
+    .split(".")[0]
+    .split("-")
+    .map((val) => {
+      if (val === "and") return "&";
+      return val[0].toUpperCase() + val.substring(1, val.length);
+    })
+    .join(" ");
+  const courseSlug = courseName.split(" ").join("-").toLowerCase();
+  document.getElementById("courseName").innerHTML = courseName.includes(
+    "Ap Computer Science"
+  )
+    ? courseName.replace("Ap Computer Science", "AP CS")
+    : courseName;
+  const public = `https://cs.meritacademy.tech/#/disclosure?name=${courseSlug}`;
+  let url = Object.keys(courses).includes(courseSlug)
+    ? `https://meritacademy.instructure.com/courses/${courses[courseSlug]}/pages/disclosure-document`
+    : public;
+  try {
+    await fetch(url, {
+      method: "HEAD",
+      mode: "no-cors",
+    });
+  } catch (e) {
+    return public;
+  }
+
+  return url;
+};
+
+getCanvasUrl()
+  .then((url) => {
+    const disclosure = document.getElementById("disclosure");
+    disclosure && disclosure.setAttribute("href", url);
+  })
+  .catch((e) => {
+    console.error(e);
+  });
